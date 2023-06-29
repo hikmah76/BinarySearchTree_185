0a122dcd7c159ebffab0b42b3acdf789d08aad3b
nur hikmah <nur.hikmah190504@gmail.com>
2023-06-29 12:58:40 +0700

    cout << "4. Perform postorder traversal" << endl;         cout << "5. Exit" << endl;         cout << "\nEnter your choice (1-5) : ";     }

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -115,5 +115,6 @@ int main()
         cout << "3. Perform preorder traversal" << endl;
         cout << "4. Perform postorder traversal" << endl;
         cout << "5. Exit" << endl;
+        cout << "\nEnter your choice (1-5) : "; 
     }
 }
