6785aa893aa0ca61896a28862ae4212431364695
nur hikmah <nur.hikmah190504@gmail.com>
2023-06-29 12:57:11 +0700

    cout << "\nMenu" << endl;         cout << "1. implement insert operation" << endl;         cout << "2. Perform inorder traversal" << endl;         cout << "3. Perform preorder traversal" << endl;

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -114,5 +114,6 @@ int main()
         cout << "2. Perform inorder traversal" << endl; 
         cout << "3. Perform preorder traversal" << endl;
         cout << "4. Perform postorder traversal" << endl;
+        cout << "5. Exit" << endl;
     }
 }
