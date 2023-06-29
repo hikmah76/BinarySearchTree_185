8e63600030a8b34d019da53dbe530acbe4cbbe25
nur hikmah <nur.hikmah190504@gmail.com>
2023-06-29 12:13:24 +0700

    void insert(string element) //Insert a node in the binary search tree     {         Node* newNode = new Node(element, NULL, NULL); //Allocate memory for the new node         newNode->info = element; // Assign value to the data field of the new node         newNode->leftchild = NULL; // Make the left cild of the new node point to NULL         newNode->rightchild = NULL; // Make the right cild of the new node point to NULL

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -31,6 +31,23 @@ public:
     }
     void insert(string element) //Insert a node in the binary search tree
     {
+        Node* newNode = new Node(element, NULL, NULL); //Allocate memory for the new node
+        newNode->info = element; // Assign value to the data field of the new node
+        newNode->leftchild = NULL; // Make the left cild of the new node point to NULL
+        newNode->rightchild = NULL; // Make the right cild of the new node point to NULL
 
+        Node* parent = NULL;
+        Node* currentNode = NULL;
+        search(element, parent, currentNode); // Locate the node which be the parent of  the node to be inserted
+
+        if (parent == NULL) // if the parent is NULL (Tree is empty)
+        {
+            ROOT = newNode; //Mark the new node as ROOT
+            return; // Exit
+            }
+        if (element < parent->info)// if the in the data field of the new node is less than that of the parent
+        {
+            parent->leftchild = newNode; // make the left cild of the parent point to the new node
+        }
     }
 };
